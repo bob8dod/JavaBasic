57027246ce19746a494606e318977e25839bfb72
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-02-11 20:05:43 +0900

    프로그래머스 고득점 Kit

--- Heap/#42626.java
+++ Heap/#42626.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 public class P42626 {
     public static int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // PriorityQueue : 우선순위 큐 -> 값을 넣을 때마다 자동으로 이진트리로 정렬. 가장 위의 값이 최솟값.
         for(int i : scoville) pq.add(i);
 
         while(pq.peek() < K){
@@ -14,8 +14,8 @@ public class P42626 {
                 return answer;
             }
 
-            int tem = pq.poll() + pq.poll()*2;
-            pq.add(tem);
+            int tem = pq.poll() + pq.poll()*2; // poll -> 가장 위의 값을 뽑아내는
+            pq.add(tem); // add -> 넣어주면서 이진트리로 정렬해줌.
             answer++;
         }
 
