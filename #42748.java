2d3b27b4dd57798828cf21ccfd96ac26f5df3d1f
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-02-11 20:07:01 +0900

    프로그래머스 고득점 Kit #42748

---
+++ #42748.java
@@ -0,0 +1,27 @@
+package algorithm.sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class P42748 {
+    public int[] solution(int[] array, int[][] commands) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int[] command : commands){
+            int size = command[1] - command[0] + 1;
+            int[] tem = new int[size];
+            int j = 0;
+            for(int i = command[0]-1; i<command[1]; i++, j++){
+                tem[j] = array[i];
+            }
+            arr.add(Arrays.stream(tem).sorted().toArray()[command[2] - 1]);
+        }
+//        System.out.println(arr);
+        int[] answer= arr.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        P42748 p = new P42748();
+        p.solution(new int[] {1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}});
+    }
+}
