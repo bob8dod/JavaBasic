a9c9ebcff0f562f171b597f276602927538772cb
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2022-02-19 18:07:27 +0900

    프로그래머스

---
+++ ETC/P11446.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class P11446 {
+    public static int solution(int[] arr) {
+        int min = 100000000;
+        int res = 0;
+        for(int i = 0; i<=255; i++){
+            int less = 0;
+            int more = 0;
+            for(int j = 0; j<arr.length; j++){
+                if (arr[j] < i) {
+                    less += 1;
+                }
+                else if (arr[j] >= i){
+                    more += 1;
+                }
+            }
+            int result = Math.abs(less-more);
+            if (result < min) {min = result; res = i;}
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int mid = solution(new int[] {1,52,125,10,25,201,244,192,128,23,203,98,154,255});
+        System.out.println("mid = " + mid);
+    }
+}
